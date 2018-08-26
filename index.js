f20bb724a35951d6f0e2d1edfe4b1cd07729fb96
Stefan Thoeni <stefan.thoeni@piratenpartei.de>
2018-08-26 02:45:10 +0200

    WIP: menu works

--- index.js
+++ index.js
@@ -3,45 +3,17 @@ var settings = require('ep_etherpad-lite/node/utils/Settings');
 exports.clientVars = function(hook, context, callback){
   var ep_group_access = {};
   try {
-    if (settings.ep_group_access){
+    if (settings.ep_group_access) {
        if(!settings.ep_group_access.link){
          console.warn("No link set for ep_group_access, add ep_group_access.link to settings.json");
          ep_group_access.link = "https://github.com/JohnMcLear/ep_group_access";
        }else{
          ep_group_access.link = settings.ep_group_access.link;
        }
-       if(!settings.ep_group_access.text){
-         ep_group_access.text = "NO TEXT SET";
-         console.warn("No text set for ep_group_access, add ep_group_access.text to settings.json");
-       }else{
-         ep_group_access.text = settings.ep_group_access.text;
-       }
-       if(!settings.ep_group_access.before){
-         ep_group_access.before = "#timesliderlink";
-         console.info("No before set for ep_group_access, this may be intentional, add ep_group_access.before to settings.json");
-       }else{
-         ep_group_access.before = settings.ep_group_access.before;
-       }
-       if(!settings.ep_group_access.classes){
-         ep_group_access.classes = "grouped-left";
-         console.info("No classes set for ep_group_access, this may be intentional, add ep_group_access.classes to settings.json");
-       }else{
-         ep_group_access.classes = settings.ep_group_access.classes;
-       }
-       if(!settings.ep_group_access.after){
-         console.info("No after set for ep_group_access, this may be intentional, add ep_group_access.classes to settings.json");
-       }else{
-         ep_group_access.after = settings.ep_group_access.after;
-       }
-
-    }else{
+    } else {
       ep_group_access = {};
       ep_group_access.link = "https://github.com/JohnMcLear/ep_group_access";
-      ep_group_access.text = "NO TEXT SET";
-      ep_group_access.before = ".buttonicon-history";
-      ep_group_access.classes = "grouped-right";
       console.warn("No link set for ep_group_access, add ep_group_access.link to settings.json");
-      console.warn("No text set for ep_group_access, add ep_group_access.text to settings.json");
     }
   } catch (e){
   }
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,33 @@
 exports.postAceInit = function(hook_name, args) {
 
   // if the button already exists, don't write it again..
-  if($('#ep_group_access_span').length !== 0) return;
+  if($('#ep_group_access_button_span').length === 0) {
+    var link = clientVars.ep_group_access.link;
+    var button = "<li><a id='ep_group_access_button'><span id='ep_group_access_button_span' class='buttonicon'><img id='ep_group_access_button_image' height=\"16\" width=\"16\" src=\"/static/plugins/ep_group_access/static/image/lock.png\"/></span></a></li><li class=\"separator\"></li>";
+    var $editBar = $("#editbar");
+    $editBar.contents().find(".buttonicon-import_export").parent().parent().before(button);
+    $('#ep_group_access_button_span').css({"background-image":"none", "width": "auto", "color":"#666", "font-size":"16px", "margin-top":"-3px"});
+    window.onclick = function(event) {
+      if ((event.target.id == 'ep_group_access_button') || 
+          $(event.target).parents("#ep_group_access_button").length) {
+        var display = window.getComputedStyle(document.getElementById('ep_group_access_popup')).getPropertyValue('display');
+        if (display === 'none') {
+          $('#ep_group_access_popup').css({ "display":"block"});
+        } else {
+          $('#ep_group_access_popup').css({ "display":"none"});
+        }
+      } else if ((event.target.id == 'ep_group_access_popup') || 
+                 $(event.target).parents("#ep_group_access_popup").length) {
+      } else {
+        $('#ep_group_access_popup').css({ "display":"none"});
+      }
+    };
+  }
 
-  var link = clientVars.ep_group_access.link;
+  if($('#ep_group_access_popup').length === 0) {
+    var box = "<div id='ep_group_access_popup' class='popup'><h2>Group Access</h2></div>";
+    $('#editorcontainerbox').after(box);
+    $('#ep_group_access_popup').css({ "position":"absolute", "right":"16px", "top":"128px", "width":"500px", "height":"300px", "z-index":"500", "display":"none"});
+  }
 
-  var button = "<li><a target='_blank' href='"+link+"'><span id='ep_group_access_span' class='buttonicon'><img height=\"16\" width=\"16\" src=\"/static/plugins/ep_group_access/static/image/lock.png\"/></span></a></li><li class=\"separator\"></li>";
-  var $editBar = $("#editbar");
-
-  $editBar.contents().find(".buttonicon-import_export").parent().parent().before(button);
-
-  $('#ep_group_access_span').css({"background-image":"none", "width": "auto", "color":"#666", "font-size":"16px", "margin-top":"-3px"});
 }
